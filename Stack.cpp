6b8aeb02728610989542c612a97c83abc01e4e60
SaberOrien <115357207+SaberOrien@users.noreply.github.com>
2022-10-23 20:33:53 +0200

    Add files via upload

---
+++ Stack.cpp
@@ -0,0 +1,112 @@
+#include "Stack.h"
+#include <iostream>
+
+using namespace std;
+
+Stack::Stack()
+{
+	this->top = 0;
+	this->size = 0;
+	this->data = NULL;
+}
+
+Stack::~Stack()
+{
+	free(this->data);
+}
+
+Stack::Stack(const Stack& s)
+{
+	size = s.size;
+	top = s.top;
+	if (size > 0) {
+		data = (int*)malloc(size * sizeof(int));
+		if (!data) {
+			cout << "Unsuccessfull copy constructor operation. \nAborting"
+				 << endl;
+			abort();
+		}
+	}
+	else {
+		data = NULL;
+	}
+	for (size_t i = 0; i < size; i++) {
+		data[i] = s.data[i];
+	}
+}
+
+Stack& Stack::operator=(const Stack& s)
+{
+	if (this != &s) {
+		if (top != s.top) {
+			free(data);
+			size = s.size;
+			top = s.top;
+			if (size > 0) {
+				data = (int*)malloc(size * sizeof(int));
+				if (!data) {
+					cout << "Unsuccessfull copy constructor operation. "
+							"\nAborting"
+						 << endl;
+					abort();
+				}
+			}
+			else {
+				data = NULL;
+			}
+		}
+		for (size_t i = 0; i < size; i++) {
+			data[i] = s.data[i];
+		}
+	}
+	return *this;
+}
+
+bool Stack::isEmpty()
+{
+	return this->top == 0;
+}
+
+void Stack::push(int element)
+{
+	manageMemory();
+	this->data[this->top++] = element;
+}
+
+void Stack::manageMemory()
+{
+	if (this->top >= this->size) {
+		size_t newSize;
+		int* newData;
+
+		if (this->size == 0) {
+			newSize = GROWTH_FACTOR;
+		}
+		else {
+			newSize = this->size + GROWTH_FACTOR;
+		}
+
+		if (newSize >= (((size_t)-1) - GROWTH_FACTOR) / sizeof(int)) {
+			cout << "Not enough memory, aborting.\n" << endl;
+			abort();
+		}
+
+		newData = (int*)realloc(this->data, newSize * sizeof(int));
+		if (!newData) {
+			cout << "Unsuccessful expansion. Aborting\n" << endl;
+			abort();
+		}
+		cout << "Stack expansion: " << this->size << " -> " << newSize << endl;
+		this->data = newData;
+		this->size = newSize;
+	}
+}
+
+int Stack::pop()
+{
+	if (isEmpty()) {
+		cout << "Popping from an empty stack.\n" << endl;
+		abort();
+	}
+	return this->data[--(this->top)];
+}
